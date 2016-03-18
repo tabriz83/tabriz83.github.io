e0b247b871544b61acb0ba5c81d0ea2f744642a0
tabriz83 <tabriz83@gmail.com>
2016-03-18 02:08:49 +0000

    구름IDE 맞게 수정

--- main.py
+++ main.py
@@ -8,77 +8,77 @@ import time
 #import facebook
 
 
-def httprequest(url) :
-    req = urllib.request.Request(url)
-    resp = urllib.request.urlopen(req)
-    content = resp.read().decode('utf-8')
-    content1 = json.loads(content)
+def httprequest(url):
+	req = urllib2.request.Request(url)
+	resp = urllib2.request.urlopen(req)
+	content = resp.read().decode('utf-8')
+	content1 = json.loads(content)
+	
+	g_data = (content1.get('data'))
 
-    g_data = (content1.get('data'))
+	i = 0
+	items = ['name', 'picture', 'description', 'link', 'updated_time']
 
-    i = 0
-    items = ['name', 'picture', 'description', 'link', 'updated_time']
-    while i < len(g_data):
-        for item in items:
-            try:
-                result = (g_data[i][item])
-                if 'name' in item:
-                    f.write("###"+result)
-                    f.write("\n\n")
-                elif 'picture' in item:
-                    f.write("!["+result+"]("+result+")")
-                    f.write("\n\n")
-                elif 'description' in item:
-                    f.write(">"+result)
-                    f.write("\n\n")
-                elif 'link' in item:
-                    f.write("**Link : <"+result+">**")
-                    f.write("\n\n")
-                elif 'updated_time' in item:
-                    f.write(result)
-                    f.write("\n\n")
+	while i < len(g_data):
+		for item in items:
+			try:
+				result = (g_data[i][item])
+				if 'name' in item:
+					f.write("###"+result)
+					f.write("\n\n")
+				elif 'picture' in item:
+					f.write("!["+result+"]("+result+")")
+					f.write("\n\n")
+				elif 'description' in item:
+					f.write(">"+result)
+					f.write("\n\n")
+				elif 'link' in item:
+					f.write("**Link : <"+result+">**")
+					f.write("\n\n")
+				elif 'updated_time' in item:
+					f.write(result)
+					f.write("\n\n")
+							
+			except KeyError as e:
+				pass
 
-            except KeyError as e:
-                pass
+	f.write("---\n\n")
 
-        f.write("---\n\n")
+	i = i + 1
 
-        i = i + 1
-
-    return content
+	return content
 
 
 def facebook_login():
-    fb.login()
-
+	fb.login()
 
 if __name__ == "__main__":
-    dt = str(datetime.date.today())
-    now_b = time.localtime()
-    now = str("%04d-%02d-%02d %02d:%02d:%02d" % (now_b.tm_year, now_b.tm_mon, now_b.tm_mday, now_b.tm_hour, now_b.tm_min, now_b.tm_sec))
-
-
-    app_id = '254922358182758'
-    app_secret = '63c3bf7253e40c7c7f851f7d78ec6916'
-    access_token = 'CAACEdEose0cBAFMmAYwEdqyDurNXZCBPBAuW9Ys011wTfHGnW16WULhZCIv5JdKDVjcLmn2wRNWEymzDNZATIM73Xmzjh0O6rBUHVosYu7wE2MiVGOjE2DvzFy4vIYXcRFZB093T05qV2KQGvIeME7Op6CTDu3ujq49vee5mMdKatdZCcYe55jZCqvoXy0nN8hbdnZCK8eAvwZDZD'
-    #url = "https://graph.facebook.com/v2.5/me/feed?fields=name%2Clink%2Cdescription%2Cpicture%2Cupdated_time&since=2016-03-15&limit=5000&locale=kor&access_token="+access_token
-    url = "https://graph.facebook.com/v2.5/me/feed?fields=name%2Clink%2Cdescription%2Cpicture%2Cupdated_time&since="+dt+"&limit=5000&locale=kor&access_token="+access_token
-
-
-    f = open("d:/Github/Pages/umbrain/_posts/"+dt+"-Facebook_scrap.md", 'w', encoding='UTF-8')
-    f.write(
-        "---\n"
-        "layout: post\n"
-        "cover: 'assets/images/cover3.jpg'\n"
-        "title: "+dt+" Facebook 정보 스크랩\n"
-        "date: "+now+"\n"
-        "tags: Unclassified\n"
-        "subclass: 'post tag-Unclassified'\n"
-        "categories: 'tabris'\n"
-        "navigation: True\n"
-        "logo: 'assets/images/logo.png'\n"
-        "---\n"
-        "\n"
-    )
-    httprequest(url)
-    f.close()
+	dt = str(datetime.date.today())
+	now_b = time.localtime()
+	now = str("%04d-%02d-%02d %02d:%02d:%02d" % (now_b.tm_year, now_b.tm_mon, now_b.tm_mday, now_b.tm_hour, now_b.tm_min, now_b.tm_sec))
+	
+	app_id = '254922358182758'
+	app_secret = '63c3bf7253e40c7c7f851f7d78ec6916'
+	access_token =  'CAACEdEose0cBAHVKTZBydZC8dQxmVXl1CZAiZCOoOZCYniTkE04eKvoKODmqGf3DPMfeTV1dbOj4sitKMP5bjYMTvxEUAQzIWXsJPgzlyNdZC2t0BsuCyrM9SrrpqUUASQn9XakMjUPWRxUE4ZACEZAEkVoe6sIGTtKEXt0WMOzije7cBFYIfZAx5BwYcktYo4ZCTvPTjna3USDgZDZD'
+	url = "https://graph.facebook.com/v2.5/me/feed?fields=name%2Clink%2Cdescription%2Cpicture%2Cupdated_time&since=2016-03-17&limit=5000&locale=kor&access_token="+access_token
+	# url = "https://graph.facebook.com/v2.5/me/feed?fields=name%2Clink%2Cdescription%2Cpicture%2Cupdated_time&since="+dt+"&limit=5000&locale=kor&access_token="+access_token
+	
+	# f = open("d:/Github/Pages/umbrain/_posts/"+dt+"-Facebook_scrap.md", 'w', encoding='UTF-8')
+	f = open("../2016-03-17-Facebook_scrap.md", 'w', encoding='UTF-8')
+	f.write(
+		"---\n"
+		"layout: post\n"
+		"cover: 'assets/images/cover3.jpg'\n"
+		# "title: "+dt+" Facebook 정보 스크랩\n"
+		"title: 2016-03-17 Facebook 정보 스크랩\n"
+		"date: "+now+"\n"
+		"tags: Unclassified\n"
+		"subclass: 'post tag-Unclassified'\n"
+		"categories: 'tabris'\n"
+		"navigation: True\n"
+		"logo: 'assets/images/logo.png'\n"
+		"---\n"
+		"\n"
+		)
+	httprequest(url)
+	f.close()
